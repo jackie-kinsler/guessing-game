9884cd57aad94768f047f57518c1c9660e459747
Jackie Kinsler <jackiekinsler@gmail.com>
2020-09-22 00:00:35 +0000

    Random number generated.

--- game.py
+++ game.py
@@ -1,5 +1,16 @@
+from random import randint
 """A number-guessing game."""
 
 # Put your code here
 
 print("I'll guess your number... give me a number between 1 and 100.")
+ 
+player_name = input("What's your name? ")
+
+guess = -100  #initialize guess value 
+
+print(player_name)
+
+number = randint(1,100)
+print(number)
+# print(type(number))
